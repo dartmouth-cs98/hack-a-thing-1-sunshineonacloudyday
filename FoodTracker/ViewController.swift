f31a9fbd54c5d34d8965b57562dd6168a3db570c
Cristina Curcelli <cristina.curcelli@gmail.com>
2017-09-13 16:46:53 -0400

    started app

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,17 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+    }
+    
+    //Cristina: If there is an issue later on, check these lines - not in tutorial ex.
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
